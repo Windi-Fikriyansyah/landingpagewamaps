f863bf95ed92c1024ec1ba6a5e0528c099b3654b
windi fikriyansyah <diwin321@gmail.com>
2026-04-22 15:59:42 +0700

    baru12

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -125,11 +125,10 @@
         }(window, document, 'script',
             'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '2691160627922371');
-        fbq('track', 'PageView');
         fbq('track', 'InitiateCheckout');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=2691160627922371&ev=PageView&noscript=1" /></noscript>
+            src="https://www.facebook.com/tr?id=2691160627922371&ev=InitiateCheckout&noscript=1" /></noscript>
     <!-- Crisp Live Chat -->
     @if (config('services.crisp.id'))
         <script type="text/javascript">
